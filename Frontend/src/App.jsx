f5ba4d1f594158f6170671de775bdacaf1222ac2
likhithb08 <likhithb285@gmail.com>
2025-03-22 08:55:01 +0530

    added landing page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Login from './components/Login'
 import Register from './components/Register'
+import LandingPage from './components/LandingPage'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />   {/* Add this */}
         <Route path="/register" element={<Register />} />   {/* Rename from /about to /register */}
       </Routes>
